52bc06d196bf721e30300f23cc7e25c05994d61c
leonardo-lin <st950065@yahoo.com.tw>
2024-08-29 17:29:39 +0800

    kyc計分方式完成
    kyc計分方式完成

--- test.py
+++ test.py
@@ -1,9 +1,3 @@
-import pandas as pd
-import os
-# filepath = os.path.join('./competitions', 'text.xlsx')
-filepath = "./competitions/test.xlsx"    
-df = pd.read_excel(filepath)
-
-# 查找第6行（索引5）沒有內容的第一列
-row = df[(df.iloc[:, 4].isna()) | (df.iloc[:, 5].isna())].iloc[0]
-print(row)
+a ='內湖 國小'
+a=a.split(' ')
+print(a)
